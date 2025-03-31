80e3c5cf26a8435e16183e77ddbfba8170846605
ThureinH-cloud <thureinh64@gmail.com>
2025-03-31 20:32:46 +0630

    Getting Started with Native Wind

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View ,Text} from 'react-native'
+
+export default function Index() {
+  return (
+    <View style={{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+    }}>
+        <Text className='font-bold'>Thurein</Text>
+    </View>
+  )
+}
